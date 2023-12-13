98f121ff2442c7ea404454fd2c924181b91f9898
arpit jain <jain.arpit@northeastern.edu>
2023-12-13 17:51:03 -0500

    useState code and description in App.js

--- src/App.js
+++ src/App.js
@@ -5,16 +5,27 @@ import FoodItems from "./components/FoodItems";
 import ErrorMessage from "./components/ErrorMessage";
 import Container from "./components/Container";
 import FoodInput from "./components/FoodInput";
+import { useState } from "react";
 
 function App() {
   // Parent - fooditems yahi define hoga === props (parent to child) one way data passing
 
   let fooditems = ["dal", "khana", "Green Vegetable"];
 
-  let textToShow = "Food item entered by user"
+  // let textStateArr = useState("Food item entered by user");  // ye array return krta hai jisme 2 elements hote hai 
+  // // pehle element hota hai curent value aapke state ki aur doosra ek method jisse app usko edit kr sakte hai.
+  // // as below
+  // let textToShow = textStateArr[0];
+  // let setTextState = textStateArr[1];
+
+  // destructuring lines 15 to 19 in one line as below
+  let [textToShow, setTextState ] = useState("Food item entered by user");
+  console.log(`Current value of textState : ${textToShow}`)
+
+  //let textToShow = "Food item entered by user"
   const handleOnChange = (event) => {
     console.log(event.target.value);
-    textToShow = event.target.value;
+    setTextState(event.target.value);
   };
 
   return (
